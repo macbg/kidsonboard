4a2d995fd7bada7169fbff39fb8d152318cd368e
Krasimir Markov <core.editor>
2021-10-26 17:11:28 +0300

    Added config section

--- child-car-seat-selector/child-car-seat-selector.php
+++ child-car-seat-selector/child-car-seat-selector.php
@@ -56,4 +56,176 @@ function seatselector_func($atts)
 add_shortcode('seatselector', 'seatselector_func');
 
 /* Stop Adding Functions Below this Line */
+
+/**
+ * custom option and settings
+ */
+function ccss_settings_init() {
+    // Register a new setting for "ccss" page.
+    register_setting( 'ccss', 'ccss_options' );
+
+    // Register a new section in the "ccss" page.
+    add_settings_section(
+        'ccss_section_main',
+        __( 'Main', 'ccss' ), 'ccss_section_main_callback',
+        'ccss'
+    );
+ 
+    // Register a new field in the "ccss_section_main" section, inside the "ccss" page.
+    add_settings_field(
+        'ccss_field_default_page', // As of WP 4.6 this value is used only internally.
+                                // Use $args' label_for to populate the id inside the callback.
+            __( 'Default page', 'ccss' ),
+        'ccss_field_cb',
+        'ccss',
+        'ccss_section_main',
+        array(
+            'label_for'         => 'ccss_field_default_page',
+        )
+    );
+
+    // Register a new field in the "ccss_section_main" section, inside the "ccss" page.
+    add_settings_field(
+        'ccss_field_page_1', // As of WP 4.6 this value is used only internally.
+                                // Use $args' label_for to populate the id inside the callback.
+            __( 'Page 1', 'ccss' ),
+        'ccss_field_cb',
+        'ccss',
+        'ccss_section_main',
+        array(
+            'label_for'         => 'ccss_field_page_1',
+        )
+    );
+
+    // Register a new field in the "ccss_section_main" section, inside the "ccss" page.
+    add_settings_field(
+        'ccss_field_page_2', // As of WP 4.6 this value is used only internally.
+                                // Use $args' label_for to populate the id inside the callback.
+            __( 'Page 2', 'ccss' ),
+        'ccss_field_cb',
+        'ccss',
+        'ccss_section_main',
+        array(
+            'label_for'         => 'ccss_field_page_2',
+        )
+    );
+
+    // Register a new field in the "ccss_section_main" section, inside the "ccss" page.
+    add_settings_field(
+        'ccss_field_page_3', // As of WP 4.6 this value is used only internally.
+                                // Use $args' label_for to populate the id inside the callback.
+            __( 'Page 3', 'ccss' ),
+        'ccss_field_cb',
+        'ccss',
+        'ccss_section_main',
+        array(
+            'label_for'         => 'ccss_field_page_3',
+        )
+    );
+
+    // Register a new field in the "ccss_section_main" section, inside the "ccss" page.
+    add_settings_field(
+        'ccss_field_page_4', // As of WP 4.6 this value is used only internally.
+                                // Use $args' label_for to populate the id inside the callback.
+            __( 'Page 4', 'ccss' ),
+        'ccss_field_cb',
+        'ccss',
+        'ccss_section_main',
+        array(
+            'label_for'         => 'ccss_field_page_4',
+        )
+    );
+}
+ 
+/**
+ * Register our wporg_settings_init to the admin_init action hook.
+ */
+add_action( 'admin_init', 'ccss_settings_init' );
+
+/**
+ * Main section callback function.
+ *
+ * @param array $args  The settings array, defining title, id, callback.
+ */
+function ccss_section_main_callback( $args ) { }
+
+////
+/**
+ * Main section field callback function.
+ *
+ * WordPress has magic interaction with the following keys: label_for, class.
+ * - the "label_for" key value is used for the "for" attribute of the <label>.
+ * - the "class" key value is used for the "class" attribute of the <tr> containing the field.
+ * Note: you can add custom key value pairs to be used inside your callbacks.
+ *
+ * @param array $args
+ */
+function ccss_field_cb( $args ) {
+    // Get the value of the setting we've registered with register_setting()
+    $options = get_option( 'ccss_options' );
+    ?>
+    <input id="<?php echo esc_attr( $args['label_for'] ); ?>" value="<?php echo esc_attr( $options[ $args['label_for'] ] ); ?>" name="ccss_options[<?php echo esc_attr( $args['label_for'] ); ?>]"/>
+    <?php
+}
+////
+
+///
+
+/**
+ * Add the top level menu page.
+ */
+function ccss_options_page() {
+    add_menu_page(
+        'Car Child Seat Selector',
+        'Seat Selector',
+        'manage_options',
+        'ccss',
+        'ccss_options_page_html'
+    );
+}
+ 
+ 
+/**
+ * Register our ccss_options_page to the admin_menu action hook.
+ */
+add_action( 'admin_menu', 'ccss_options_page' );
+
+/**
+ * Top level menu callback function
+ */
+function ccss_options_page_html() {
+    // check user capabilities
+    if ( ! current_user_can( 'manage_options' ) ) {
+        return;
+    }
+ 
+    // add error/update messages
+ 
+    // check if the user have submitted the settings
+    // WordPress will add the "settings-updated" $_GET parameter to the url
+    if ( isset( $_GET['settings-updated'] ) ) {
+        // add settings saved message with the class of "updated"
+        add_settings_error( 'ccss_messages', 'ccss_message', __( 'Settings Saved', 'ccss' ), 'updated' );
+    }
+ 
+    // show error/update messages
+    settings_errors( 'ccss_messages' );
+    ?>
+    <div class="wrap">
+        <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+        <form action="options.php" method="post">
+            <?php
+            // output security fields for the registered setting "ccss"
+            settings_fields( 'ccss' );
+            // output setting sections and their fields
+            // (sections are registered for "ccss", each field is registered to a specific section)
+            do_settings_sections( 'ccss' );
+            // output save settings button
+            submit_button( 'Save Settings' );
+            ?>
+        </form>
+    </div>
+    <?php
+}
+
 ?>
