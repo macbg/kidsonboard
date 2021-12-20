da89bccc084925973b933eebad8121580fac4fec
Krasimir Markov <core.editor>
2021-12-20 17:36:02 +0200

    added jquery-ui-touch-punch to add support for mobile devices

--- child-car-seat-selector/child-car-seat-selector.php
+++ child-car-seat-selector/child-car-seat-selector.php
@@ -3,7 +3,7 @@
  * @wordpress-plugin
  * Plugin Name: Child Car Seat Selector
  * Description: Child car seat selector widget. It redirects to a specific wordpress info page, based on the provided age, wieght and height.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Krasimir Markov
  * Author URI: https://www.linkedin.com/in/krasimir-markov
  * License:           GPL-2.0+
@@ -14,10 +14,11 @@
 /* Start Adding Functions Below this Line */
 
 $plugin_name = "child-car-seat-selector";
-$plugin_version = "1.0.0";
+$plugin_version = "1.0.1";
 
 wp_enqueue_style( $plugin_name, plugin_dir_url( __FILE__ ) . 'public/css/child-car-seat-selector.css', array(), $plugin_version, 'all' );
 wp_enqueue_script( $plugin_name, plugin_dir_url( __FILE__ ) . 'public/js/child-car-seat-selector.js', array( 'jquery', 'jquery-ui-button', 'jquery-ui-slider' ), $plugin_version, true );
+wp_enqueue_script( 'jquery-ui-touch-punch', plugin_dir_url( __FILE__ ) . 'public/js/jquery.ui.touch-punch.min.js', array(), $plugin_version, true );
 
 
 //[seatselector]
